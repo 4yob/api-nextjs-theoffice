ebb75db853843a0d916d25a5a594961af944e475
Alejandra Barros <alejandra.barros@aluno.senai.br>
2025-09-16 16:21:22 -0300

    refactor: restart homepage

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -4,38 +4,11 @@ import styles from './home.module.css';
 
 export default function Home() {
     return (
-        <div className={styles.homeContainer}>
-            <div className={styles.homeContent}>
-                <div className={styles.homeCard}>
-                    <div className={styles.badge}>
-                        Projeto TCC  <span className={styles.span}>•</span> FRONT-END <span className={styles.span}>•</span> 2TDS1
-                    </div>
-                    <h1 className={styles.title}>
-                        THE OFFICE API
-                    </h1>
-                    <p className={styles.description}>
-                        Seja bem-vindo(a) ao projeto de<span className={styles.span}>Alejandra Barros</span>. Este site foi criado para armazenar e apresentar o trabalho de TCC sobre os personagens da série de The Office.
-                    </p>
-                    <div className={styles.buttonContainer}>
-                        <Link
-                            href="/about"
-                            className={styles.primaryButton}
-                        >
-                            Conheça o Projeto
-                        </Link>
-                        <Link
-                            href="/api"
-                            className={styles.secondaryButton}
-                        >
-                            API
-                        </Link>
-                        <div className={styles.cardFooter}>
-                            <p className={styles.footerText}>
-                                © {new Date().getFullYear()} Projeto TCC – Desenhos Animados 2D
-                            </p>
-                        </div>
-                    </div>
-                </div>
+        <div className={styles.container}>
+            <h1 className={styles.title}>the office</h1>
+            <div className={styles.buttonContainer}>
+                <Link href="/api" className={styles.button}>Ir para a API</Link>
+                <Link href="/api" className={styles.button}>Conheça o projeto</Link>
             </div>
         </div>
     );
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,9 +1,22 @@
 import "./globals.css";
+import localFont from "next/font/local";
+
+const robotoMono = localFont({
+    src: [
+        {
+            path: "../../public/fonts/RobotoMono-Medium.ttf",
+            weight: "600",
+            style: "normal",
+        },
+    ],
+    variable: "--font-roboto-mono",
+    display: "swap",
+});
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
-      <body>
+      <body className={robotoMono.variable}>
         {children}
       </body>
     </html>
